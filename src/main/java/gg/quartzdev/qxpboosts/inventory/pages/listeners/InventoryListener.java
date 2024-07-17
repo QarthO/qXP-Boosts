7e973e38e3779f7050dfd3fb98f80afedadb4c2c
QarthO <qartho@gmail.com>
2024-07-17 03:39:47 -0400

    clean up inventory checks

--- src/main/java/gg/quartzdev/qxpboosts/inventory/pages/listeners/InventoryListener.java
+++ src/main/java/gg/quartzdev/qxpboosts/inventory/pages/listeners/InventoryListener.java
@@ -5,12 +5,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryType;
 
 public class InventoryListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event){
         if(!(event.getInventory().getHolder() instanceof SettingsInventory)){
+            return;
+        }
+        if(event.getClickedInventory() == null || event.getClickedInventory().getType() == InventoryType.PLAYER){
+            return;
+        }
+        if(event.getCurrentItem() == null){
             return;
         }
         ((SettingsInventory) event.getInventory().getHolder()).onClick(event);
